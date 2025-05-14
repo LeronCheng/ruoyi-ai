7045bdb5cb5fbfc698f60fba788dbe932b35c7f4
yycc <1628121409@qq.com>
2025-05-14 16:43:05 +0800

    企业微信回调接口

--- ruoyi-modules/ruoyi-wechat/src/main/java/org/ruoyi/controller/WxPortalController.java
+++ ruoyi-modules/ruoyi-wechat/src/main/java/org/ruoyi/controller/WxPortalController.java
@@ -6,6 +6,8 @@ import me.chanjar.weixin.cp.bean.message.WxCpXmlMessage;
 import me.chanjar.weixin.cp.bean.message.WxCpXmlOutMessage;
 import me.chanjar.weixin.cp.util.crypto.WxCpCryptUtil;
 import org.apache.commons.lang3.StringUtils;
+import org.ruoyi.aes.AesException;
+import org.ruoyi.aes.WXBizMsgCrypt;
 import org.ruoyi.common.core.utils.JsonUtils;
 
 import org.ruoyi.config.WxCpConfiguration;
@@ -25,6 +27,12 @@ public class WxPortalController {
 
     @Value("${wechat.cp.appConfigs[0].agentId}")
     private Integer agentId;
+    @Value("${wechat.cp.corpId}")
+    private String corpId;
+    @Value("${wechat.cp.appConfigs[0].token}")
+    private String sToken;
+  @Value("${wechat.cp.appConfigs[0].aesKey}")
+  private String sEncodingAESKey;
 
 
   @GetMapping(produces = "text/plain;charset=utf-8")
@@ -51,7 +59,36 @@ public class WxPortalController {
 
     return "非法请求";
   }
-
+  
+  @GetMapping("/feedbackFromWx")
+  public String feedbackFromWxVerifyURL(String msg_signature, String timestamp, String nonce, String echostr) throws AesException {
+    
+    System.out.printf("-------feedbackFromWx ------ msg_signature:%s, Long timestamp:%s, String nonce:%s, String echostr:%s%n",
+            msg_signature, timestamp, nonce, echostr);
+//    String sToken = "pnRQhOyo5epoQHpy";
+//    String sEncodingAESKey = "WcDeO4sJCx7HkP25JE6ssjydKsZ86XaqziKaTpvS9vQ";
+    
+    WXBizMsgCrypt wxcpt = new WXBizMsgCrypt(sToken, sEncodingAESKey, corpId);
+    String sMsg = wxcpt.VerifyURL(msg_signature, timestamp, nonce, echostr);
+    System.out.printf("sMsg:%s%n", sMsg);
+    return sMsg;
+  }
+  
+  @PostMapping("/feedbackFromWx")
+  public String feedbackFromWxData(String msg_signature, String timestamp, String nonce, @RequestBody String data)
+          throws Exception {
+//        System.out.printf("-------feedbackFromWx ------ msg_signature:%s, Long timestamp:%s, String nonce:%s, String data:%s%n",
+//                msg_signature, timestamp, nonce, data);
+    log.info("-------feedbackFromWx ------ msg_signature:{}, Long timestamp:{}, String nonce:{}, String data:{}%n", msg_signature, timestamp, nonce, data);
+//    String sToken = "pnRQhOyo5epoQHpy";
+//    String sEncodingAESKey = "WcDeO4sJCx7HkP25JE6ssjydKsZ86XaqziKaTpvS9vQ";
+    WXBizMsgCrypt wxcpt = new WXBizMsgCrypt(sToken, sEncodingAESKey, corpId);
+    String sMsg = wxcpt.DecryptMsg(msg_signature, timestamp, nonce, data);
+    
+    log.info("timestamp: {}, feedback message: {}", System.currentTimeMillis(), sMsg);
+    return sMsg;
+  }
+  
   @PostMapping(produces = "application/xml; charset=UTF-8")
   public String post(
                      @RequestBody String requestBody,
@@ -61,7 +98,7 @@ public class WxPortalController {
     log.info("\n接收微信请求：[signature=[{}], timestamp=[{}], nonce=[{}], requestBody=[\n{}\n] ",
         signature, timestamp, nonce, requestBody);
 
-    final WxCpService wxCpService = WxCpConfiguration.getCpService(1000002);
+    final WxCpService wxCpService = WxCpConfiguration.getCpService(agentId);
     WxCpXmlMessage inMessage = WxCpXmlMessage.fromEncryptedXml(requestBody, wxCpService.getWxCpConfigStorage(),
         timestamp, nonce, signature);
     log.debug("\n消息解密后内容为：\n{} ", JsonUtils.toJson(inMessage));
